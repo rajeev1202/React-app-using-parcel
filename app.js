e47c649d0fbfd68d653c56a23853f57da71dd45f
Rajeev Ranjan <rajeev.ranjan@routerabbit.com>
2023-01-19 18:18:53 +0530

    session-3 courseWork, functional component,browserlist,babel plugin to remove console

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Footer from './Footer';
+import {useState} from 'react';
 
 
 const heading = React.createElement(
@@ -8,8 +9,13 @@ const heading = React.createElement(
     {
         id:"title",
     },
-    ["heading first time", Footer()]
-)
+    ["heading first time", Footer()]);
+
+let Hello = (props) => {
+return <div>{console.log("useState",useState(''))}Hello i'm from {props.msg}</div>
+}
+const heading1 = <h1>Hey guys i'm from JSX</h1>
+console.log("jsx",heading1)
 
 let root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<Hello msg="JSX"/>);
